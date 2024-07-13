ff2228c6174cfdbebefdd71d827a11b6e1ff8941
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-13 22:51:41 +0545

    variable declaration with let, var and const

---
+++ day-01-variables.js
@@ -0,0 +1,18 @@
+// Day 1: Variables and Data type
+// Task/Activities:
+
+// Activity - 01: Variable Declaraction
+// var
+var challangeDay = 30;
+console.log(challangeDay);
+
+// let
+let challangeName = "ChaiCode-30-JS-Challange";
+console.log(challangeName);
+
+// Activity - 02: Constant Declaraction
+// const
+const challangeAccept = true;
+console.log(challangeAccept);
+
+// Activity - 02: Data Type
