9d0b7b55ca7d0ff4e89a2fc71fa47ac3d43a9bd7
Bhabin <poon.bhabin6332@gmail.com>
2024-07-13 23:15:14 +0545

    Spelling correction

--- day-01-variables.js
+++ day-01-variables.js
@@ -3,35 +3,35 @@
 
 // Activity - 01: Variable Declaraction
 // var
-var challangeDay = 30;
-console.log(challangeDay);
+var challengeDay = 30;
+console.log(challengeDay);
 
 // let
-let challangeName = "ChaiCode-30-JS-Challange";
-console.log(challangeName);
+let challengeName = "ChaiCode-30-JS-challenge";
+console.log(challengeName);
 
 // Activity - 02: Constant Declaraction
 // const
-const challangeAccept = true;
-console.log(challangeAccept);
+const challengeAccept = true;
+console.log(challengeAccept);
 
 // Activity - 03: Data Type
-let challangeDay1 = 1;
-let challangeDayOne = "One";
-let challangeDay1Accept = true;
-const challangeObject = {
-  challangePlatform: "chaicode",
-  challangeAcceptBy: "bhabin buduja pun",
+let challengeDay1 = 1;
+let challengeDayOne = "One";
+let challengeDay1Accept = true;
+const challengeObject = {
+  challengePlatform: "chaicode",
+  challengeAcceptBy: "bhabin buduja pun",
   challageTotalDays: 30,
 };
 
-let challangeArray = [1, true, "chaicode", "JS-30"];
+let challengeArray = [1, true, "chaicode", "JS-30"];
 
-console.log(typeof challangeDay1);
-console.log(typeof challangeDayOne);
-console.log(typeof challangeDay1Accept);
-console.log(typeof challangeObject);
-console.log(typeof challangeArray);
+console.log(typeof challengeDay1);
+console.log(typeof challengeDayOne);
+console.log(typeof challengeDay1Accept);
+console.log(typeof challengeObject);
+console.log(typeof challengeArray);
 
 // Activity - 04: Reassign Variables
 let reassignVariable = true;
@@ -50,4 +50,3 @@ console.log(constVariable);
 
 // TypeError: Assignment to constant variable.
 //     at Object.<anonymous>
-
