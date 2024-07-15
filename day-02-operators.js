c16abb971ccbfc9b148cb0c721e979bc9367d6a6
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-15 09:52:54 +0545

    day-02-operators in javascript

---
+++ day-02-operators.js
@@ -0,0 +1,50 @@
+// Day 1: Operators
+// Task/Activities:
+
+// Activity - 01: Arithmetic Operations
+// Task 1: WAP to add two numbers and log the result to the console.
+let addFirstNumber = 66;
+let addSecondNumber = 11;
+let sum = addFirstNumber + addSecondNumber;
+console.log(sum);
+
+// Task 2: WAP to sub two numbers and log the result to the console.
+let subFirstNumber = 66;
+let subSecondNumber = 11;
+let sub = subFirstNumber - subSecondNumber;
+console.log(sub);
+
+// Task 3: WAP to mul two numbers and log the result to the console.
+let mulFirstNumber = 66;
+let mulSecondNumber = 11;
+let mul = mulFirstNumber * mulSecondNumber;
+console.log(mul);
+
+// Task 4: WAP to div two numbers and log the result to the console.
+let divFirstNumber = 66;
+let divSecondNumber = 11;
+let div = divFirstNumber / divSecondNumber;
+console.log(div);
+
+// Task 4: WAP to find remainder bewteen two numbers and log the result to the console.
+let FirstNumber = 66;
+let SecondNumber = 11;
+let remainder = FirstNumber % SecondNumber;
+console.log(remainder);
+
+// // Activity - 02: Assignment Operators
+// Task 6:
+// Task 7:
+
+// // Activity - 03: Comparison Operators
+// Task 8:
+// Task 9:
+// Task 10:
+
+// // Activity - 04: Logical Operators
+// Task 11:
+// Task 12:
+// Task 13:
+
+// // Activity - 05: Ternary Operators
+// Task 14:
