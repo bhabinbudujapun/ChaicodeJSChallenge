fab0ffe32073bab6f6015a1ef7c5b82f107f7389
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-19 17:02:15 +0545

    loop challenge

---
+++ day-04-loop.js
@@ -0,0 +1,26 @@
+// Day 4: Loop
+// Task/Activities:
+
+// Activity - 01: If-Else Statements
+// Task 1: WAP to print numbers from 1 to 10 using for loop
+// Task 2: WAP to print multiplication table of 6 using a for loop
+
+// // Activity - 02: While Loop
+// Task 3: WAP to calculate sum of numbers from 1 to 10 using while loop
+// Task 4: WAP to print numbers from 10 to 1 using while loop
+
+// // Activity - 03: Do...While Loop
+// Task 5: WAP print numbers from 1 to 5 using a do...while loop
+// Task 6: WAP calculate the factorial of number using a do...while loop
+
+// // Activity - 04: Nested Loops
+// Task 7: WAP to print pattern using nested for loops
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// // Activity - 05: Loop Control Statements
+// Task 8: WAP to print number from 1 to 10, but skip number 5 using 'continue'  statement
+// Task 9: WAP to print number from 1 to 10, but stop the loop at number 7 using 'break' statement
