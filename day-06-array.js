3b98a007a246a9836b83b9c7ad5c3261e24c4e34
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-21 18:56:38 +0545

    arrays problems(questions)

---
+++ day-06-array.js
@@ -0,0 +1,25 @@
+// Day 6: Array
+// Task/Activities:
+
+// Activity - 01: Array Creation and Access
+// Task 1: Create an array of numbers from 1 to 5 and log the array values
+// Task 2: Access the first and last element of the previous array
+
+// Activity - 02: Array Methods (Basic)
+// Task 3: Use 'push' method to add new number and log updated array
+// Task 4: Use 'pop' method to remove the last element and log the updated array
+// Task 5: Use 'shift' method to remove the first element from the array log the updated array
+// Task 6: Use 'unshift' method to add new number to the beginning of the array and log the updated array
+
+// Activity - 03: Array Methods (Intermediate)
+// Task 7: Use 'map' method to double each value and log the updated value
+// Task 8: Use 'filter' method create new array of even numbers and log the new array
+// Task 9: Use 'reduce' method to sum all the numbers of array.
+
+// Activity - 04: Array Iteration
+// Task 10: Use 'for loop' to iterate over the array
+// Task 11: Use 'forEach' method to iterate over the array
+
+// Activity - 05: Multi-dimensional arrays
+// Task 12: Create a two-dimensional array (matrix) and log the entire array
+// Task 13: Access and log a specific element from the 2D array.
