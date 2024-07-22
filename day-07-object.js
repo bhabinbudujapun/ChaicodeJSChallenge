9f10f3f6ff2be4906bb8e2aa62c5723cce60a29a
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-22 15:27:12 +0545

    object challange question format

---
+++ day-07-object.js
@@ -0,0 +1,21 @@
+// Day 6: Object
+// Task/Activities:
+
+// Activity - 01: Object Creation and Access
+// Task 1:
+// Task 2:
+
+// Activity - 02: Object Methods (Basic)
+// Task 3:
+// Task 4:
+
+// Activity - 03: Nested Objects (Intermediate)
+// Task 5:
+// Task 6:
+
+// Activity - 04: The 'this' keyword
+// Task 7:
+
+// Activity - 05: Objects Iteration
+// Task 8:
+// Task 9:
