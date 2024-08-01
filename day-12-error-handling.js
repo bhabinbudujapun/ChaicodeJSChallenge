59d021125c1e80ba3824c1bfdd430a399e758348
bhabinpun <poon.bhabin6332@gmail.com>
2024-08-01 22:13:47 +0545

    Error handliing

---
+++ day-12-error-handling.js
@@ -0,0 +1,21 @@
+// Day 12: Error Handling
+// Task/Activities:
+
+// Activity - 01: Basic Error Handling with Try-Catch
+// Task 1: Write a function that intentionally throws an error and use a try-catch block to handle the error and log an appropriate message to the console.
+// Task 2: Create a function that divides two numbers and throws an error if the denominator is zero. Use a try-catch block to handle this error.
+
+// Activity - 02: Finally Block
+// Task 3: Write a script that includes a try-catch block and a finally block. Log messages in the try, catch, and finally blocks to observe the execution flow.
+
+// Activity - 03: Custom Error Objects
+// Task 4: Create a custom error class that extends the built-in Error class. Throw an instance of this customer error in a function and handle it using try-catch error  block.
+// Task 5:  Write a function that validates user input (e.g., checking if a string is not empty) and throws a custom error if the validation fails. Handle the custom error using a try-catch block/
+
+// Activity - 04: Error Handling is Promises
+// Task 6: Create a promise that randomly resloves or reject. Use .catch() to handle the rejection and log an appropriate message to the console.
+// Task 7: Use try-catch within an async function to handle errors from a promise that randomly reslove or reject, and log the error message.
+
+// Activity - 05: Graceful Error Handling in Fetch
+// Task 8: Use the fetch API to request data from an invalid URL and handle the error using .catch(). Log an appropriate error message to console.
+// Task 9: Use the fetch API to request data from an invalid URL within an async function and handle the error using try-catch. Log an appropriate error message.
