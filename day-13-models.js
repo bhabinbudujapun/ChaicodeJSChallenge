3a713d93f808fd05537a97abf8a3d08da3645902
Bhabin <poon.bhabin6332@gmail.com>
2024-08-03 21:42:06 +0545

    modules questions

---
+++ day-13-models.js
@@ -0,0 +1,20 @@
+// Day 13: Modules
+// Task/Activities:
+
+// Activity - 01: Creating And Exporting Modules
+// Task 1:Create a module that exports a function to and two number. Import and use this module in another script.
+// Task 2:Create a module that exports an object representing a person with properties and method. Import and use this module in another script.
+
+// Activity - 02: Named and Default Exports
+// Task 3: Create a module that exports multiple functions using named exports. Import and use these functions in another script.
+// Task 4: Create a module that exports a single functions using default exports. Import and use this functions in another script.
+
+// Activity - 03: Impo0rting Entire Modules
+// Task 5: Create a module that exports multiple constants and functions. Import the entire module as an object in another script and use its properties.
+
+// Activity - 04: Using Third-Party Module
+// Task 6: Install a third-party module (e.g., lodash) using npm. Import and use a function from this module in a script.
+// Task 7: Install a third-party module (e.g., axios) using npm. Import and use this module to make a network request in a script.
+
+// Activity - 05: Module Bunding (Optional)
+// Task 8: Use a module bundler like Webpack or Parcel to bundle multiple JavaSript files into a single file. Write a script to demonstrate the bundling process.
