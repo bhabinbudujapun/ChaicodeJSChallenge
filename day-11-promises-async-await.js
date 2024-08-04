bc34a4dec2b3168b90a2b0767759f9073932c52d
bhabinpun <poon.bhabin6332@gmail.com>
2024-08-04 17:47:26 +0545

    understanding promises concept

--- day-11-promises-async-await.js
+++ day-11-promises-async-await.js
@@ -3,14 +3,19 @@
 
 // Activity - 01: Understanding Promises
 // Task 1: Create a promise that resloves with a message after a 2-second timeout and log the message to the console.
+const promise = new Promise(function (reslove, reject) {
+  setTimeout(() => {
+    console.log("Hello, there this is the simple example of Promises");
+  }, 2000);
+});
 // Task 2: Create a promise that rejects with an error message after a 2-second timeout and handle the error using .catch().
 
 // Activity - 02: Chaining Promise
 // Task 3: Create a sequence of promises that simulate fetching data from a server. Chain the promises to log messages in a specific order.
 
 // Activity - 03: Using Async/Await
-// Task 4: Write an async function that waits for a promise to reslove and then logs the reloved value. 
-// Task 5: Write an async function that handles a rejected promise using try-catch and logs the error message. 
+// Task 4: Write an async function that waits for a promise to reslove and then logs the reloved value.
+// Task 5: Write an async function that handles a rejected promise using try-catch and logs the error message.
 
 // Activity - 04: Fetching Data from an API
 // Task 6: Use the fetch API to get data from a public API and log the response data to the console using promises.
