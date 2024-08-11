403f77d6f4eb49e1548e45535e2993f4b98f6f57
bhabinpun <poon.bhabin6332@gmail.com>
2024-08-11 13:47:46 +0545

    while loop problem

--- day-04-loop.js
+++ day-04-loop.js
@@ -9,13 +9,29 @@ for (let number = 1; number <= 10; number++) {
 }
 
 // Task 2: WAP to print multiplication table of 6 using a for loop
+
 for (let table = 1; table <= 10; table++) {
   console.log(table * 6);
 }
 
 // // Activity - 02: While Loop
 // Task 3: WAP to calculate sum of numbers from 1 to 10 using while loop
+
+let num = 1;
+let sum = 0;
+while (num <= 10) {
+  sum += num;
+  num += 1;
+}
+console.log(sum);
+
 // Task 4: WAP to print numbers from 10 to 1 using while loop
+
+let reverse_num = 10;
+while (reverse_num) {
+  console.log(reverse_num);
+  reverse_num--;
+}
 
 // // Activity - 03: Do...While Loop
 // Task 5: WAP print numbers from 1 to 5 using a do...while loop
