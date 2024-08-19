e89c41f5c203ba4b9bc229aa7e98c13f88ec3ed8
Bhabin <poon.bhabin6332@gmail.com>
2024-08-19 23:01:32 +0545

    question of object

--- day-07-object.js
+++ day-07-object.js
@@ -2,20 +2,20 @@
 // Task/Activities:
 
 // Activity - 01: Object Creation and Access
-// Task 1:
-// Task 2:
+// Task 1: create an object representing a book with properties like title author and year and log the object to the console
+// Task 2: access the log and the title and author properties of the book object
 
 // Activity - 02: Object Methods (Basic)
-// Task 3:
-// Task 4:
+// Task 3: add a method to the book object that return a sting with the book title and author and log the result of calling this method
+// Task 4: add a method to the book object that take a parameter(year) and update  the books year property then log the updated object
 
 // Activity - 03: Nested Objects (Intermediate)
-// Task 5:
-// Task 6:
+// Task 5: create a nested object respresenting a library with properties like name and book ( an array of book object) and log the library object to the console
+// Task 6: access tand log the name of library and the title of all the books in the libaray
 
 // Activity - 04: The 'this' keyword
-// Task 7:
+// Task 7: add a method to the book object that use the this keyword to return a string with the book title and year and log the result of calling this method
 
 // Activity - 05: Objects Iteration
-// Task 8:
-// Task 9:
+// Task 8: use a for...in loop to iterate over the properties of the book object and log each property and its value
+// Task 9: use object.key and object.value method to log alll the key value of the book object
